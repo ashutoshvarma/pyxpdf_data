ab2def0116fe19ae56e0d1b10433bcd7066e971c
Ashutosh Varma <ashutoshvarma11@live.com>
2020-08-03 16:02:07 +0530

    add basic tests

---
+++ tests/test_pyxpdf_integration.py
@@ -0,0 +1,10 @@
+import pyxpdf_data
+from pyxpdf import Config
+
+
+def test_integration():
+    assert Config.cfg_path == pyxpdf_data.get_xpdfrc(force_rewrite=False)
+
+
+def test_xpdfrc_load():
+    Config.text_encoding = "Big5"
